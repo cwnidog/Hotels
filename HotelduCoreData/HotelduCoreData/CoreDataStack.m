71a6bbd09afad38f3a7b46412023eaed6ea8390e
cwnidog <cwnidog@comcast.net>
2015-02-14 14:32:51 -0800

    Fixing context bug in the Availability VC.

--- HotelduCoreData/HotelduCoreData/CoreDataStack.m
+++ HotelduCoreData/HotelduCoreData/CoreDataStack.m
@@ -112,7 +112,19 @@
   NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"HotelduCoreData.sqlite"];
   NSError *error = nil;
   NSString *failureReason = @"There was an error creating or loading the application's saved data.";
-  if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+  
+  NSString *storeType;
+  if (self.isTesting)
+  {
+    storeType = NSInMemoryStoreType; // store in the sandbox only
+  }
+  
+  else
+  {
+    storeType = NSSQLiteStoreType; // store in persistent memory
+  }
+  
+  if (![_persistentStoreCoordinator addPersistentStoreWithType:storeType configuration:nil URL:storeURL options:nil error:&error]) {
     // Report any error we got.
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
     dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
